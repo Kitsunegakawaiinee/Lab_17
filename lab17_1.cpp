d2b6c626324a74f8b6a211cb7389973bfef462ee
SKOT <skotskip@gmail.com>
2023-02-13 23:38:59 +0700

    งานเสร็จแล้ว

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,69 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *point ,int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(point+i);
+        if((i+1) % M == 0 && M != 0 && N != 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *dPtr,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(dPtr+i) = (rand()%101)*0.01;
+    }
+}
+
+void findRowSum(const double *dPtr,double * sum1,int N,int M)
+{
+    int point = 0;
+    for(int i =0; i <N; i++) *(sum1+i) = 0;
+
+
+    //for(int i =0; i <N; i++) cout << *(sum1+i) << " ";
+    //cout << endl;
+
+    for(int i = 0; i< N*M; i++)
+    {
+        *(sum1+point) += *(dPtr + i);
+        if ((i+1) % M == 0) point +=1 ;
+    }
+}
+
+void findColSum(const double *dPtr,double * sum1,int N,int M)
+{
+    int point = 0;
+    double a[N][M];
+    
+    for(int i =0; i <M; i++) *(sum1+i) = 0;
+        
+    for (int i = 0; i< N; i++)
+    {
+        for(int j = 0; j<M; j++)
+        {
+            a[i][j] = *(dPtr+point);
+            if (point < N*M) point ++;
+        }
+    }
+
+    // for (int i = 0; i< N; i++)
+    // {
+    //     for(int j = 0; j<M; j++)
+    //     {
+    //         cout << a[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    for (int i = 0; i< M; i++)
+    {
+        //cout << "i = " << i << endl;
+        for(int j = 0; j<N; j++)
+        {
+            *(sum1+i) += a[j][i];
+        }
+    }
+}
